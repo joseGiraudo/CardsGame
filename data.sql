2723e9105126d908397636326c9abbcbc0018f09
joseGiraudo <josegiraudo7@gmail.com>
2025-03-03 12:53:11 -0300

    cards and series data population

---
+++ data.sql
@@ -0,0 +1,201 @@
+
+
+
+USE cardsgamedb;
+
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Dragon Guardian', 85, 95, 'dragon_guardian.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Shadow Assassin', 90, 40, 'shadow_assassin.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Elven Archer', 65, 50, 'elven_archer.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Stone Golem', 60, 100, 'stone_golem.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Fire Elemental', 75, 65, 'fire_elemental.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Water Sprite', 55, 70, 'water_sprite.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Knight Templar', 70, 80, 'knight_templar.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Mystic Sage', 45, 85, 'mystic_sage.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Goblin Raider', 50, 45, 'goblin_raider.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Ancient Lich', 80, 70, 'ancient_lich.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Frost Giant', 75, 90, 'frost_giant.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Spirit Wolf', 60, 60, 'spirit_wolf.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Phoenix Guardian', 85, 75, 'phoenix_guardian.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Merfolk Hunter', 65, 60, 'merfolk_hunter.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Dwarven Defender', 55, 95, 'dwarven_defender.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Celestial Angel', 70, 85, 'celestial_angel.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Demon Overlord', 95, 65, 'demon_overlord.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Forest Druid', 50, 75, 'forest_druid.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Minotaur Warrior', 80, 75, 'minotaur_warrior.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Sand Wraith', 70, 65, 'sand_wraith.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Thunder Shaman', 75, 60, 'thunder_shaman.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Iron Juggernaut', 65, 90, 'iron_juggernaut.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Vampire Lord', 80, 70, 'vampire_lord.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Crystal Guardian', 55, 85, 'crystal_guardian.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Orc Berserker', 85, 50, 'orc_berserker.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Ninja Infiltrator', 75, 55, 'ninja_infiltrator.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Mountain Troll', 70, 80, 'mountain_troll.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Fairy Enchantress', 45, 65, 'fairy_enchantress.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Barbarian Chief', 90, 60, 'barbarian_chief.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Undead Knight', 75, 75, 'undead_knight.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Sea Serpent', 85, 80, 'sea_serpent.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Royal Paladin', 70, 90, 'royal_paladin.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Dark Necromancer', 80, 65, 'dark_necromancer.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Earth Elemental', 65, 85, 'earth_elemental.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Sky Wyvern', 90, 70, 'sky_wyvern.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Jungle Hunter', 70, 65, 'jungle_hunter.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Battle Mage', 75, 70, 'battle_mage.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Ancient Construct', 60, 95, 'ancient_construct.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Shadow Dragon', 95, 85, 'shadow_dragon.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Desert Nomad', 55, 60, 'desert_nomad.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Holy Priest', 50, 80, 'holy_priest.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Chaos Imp', 65, 45, 'chaos_imp.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Silver Knight', 75, 80, 'silver_knight.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Fungal Beast', 60, 70, 'fungal_beast.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Arctic Fox', 50, 55, 'arctic_fox.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Pirate Captain', 75, 65, 'pirate_captain.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Cave Troll', 80, 85, 'cave_troll.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Star Weaver', 70, 75, 'star_weaver.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Swamp Witch', 65, 60, 'swamp_witch.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Volcanic Drake', 85, 75, 'volcanic_drake.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Steel Sentinel', 60, 90, 'steel_sentinel.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Deep Sea Kraken', 90, 80, 'deep_sea_kraken.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Moon Priestess', 65, 75, 'moon_priestess.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Corrupted Knight', 80, 70, 'corrupted_knight.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Living Tree', 55, 95, 'living_tree.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Desert Scorpion', 70, 65, 'desert_scorpion.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Frost Mage', 75, 60, 'frost_mage.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Phantom Assassin', 85, 50, 'phantom_assassin.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Mountain Giant', 75, 95, 'mountain_giant.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Soul Harvester', 80, 70, 'soul_harvester.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Lightning Elemental', 85, 60, 'lightning_elemental.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Enchanted Golem', 60, 85, 'enchanted_golem.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Jungle Shaman', 70, 75, 'jungle_shaman.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('War Chief', 85, 75, 'war_chief.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Spectral Knight', 70, 65, 'spectral_knight.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Vine Monster', 65, 80, 'vine_monster.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Arcane Scholar', 55, 70, 'arcane_scholar.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Desert Djinn', 80, 65, 'desert_djinn.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Ice Queen', 75, 80, 'ice_queen.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Dark Samurai', 85, 70, 'dark_samurai.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Rock Titan', 70, 100, 'rock_titan.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Blood Hunter', 90, 55, 'blood_hunter.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Mist Walker', 65, 70, 'mist_walker.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Sun Priest', 60, 80, 'sun_priest.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Corrupted Beast', 80, 75, 'corrupted_beast.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Noble Griffin', 75, 70, 'noble_griffin.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Ancient Sphinx', 85, 85, 'ancient_sphinx.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Swamp Horror', 80, 60, 'swamp_horror.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Mountain Scout', 60, 65, 'mountain_scout.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Lava Elemental', 85, 70, 'lava_elemental.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Forest Guardian', 65, 85, 'forest_guardian.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Cursed Warrior', 75, 70, 'cursed_warrior.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Mystic Oracle', 55, 75, 'mystic_oracle.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Sand Golem', 65, 90, 'sand_golem.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Frost Demon', 85, 65, 'frost_demon.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Shadow Priest', 70, 75, 'shadow_priest.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Iron Dwarf', 65, 85, 'iron_dwarf.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Celestial Mage', 80, 70, 'celestial_mage.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Swamp Troll', 75, 80, 'swamp_troll.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Chaos Knight', 90, 65, 'chaos_knight.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Crystal Fairy', 55, 60, 'crystal_fairy.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Spirit Healer', 50, 85, 'spirit_healer.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Jungle Stalker', 80, 60, 'jungle_stalker.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Tribal Shaman', 65, 75, 'tribal_shaman.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Emerald Dragon', 90, 85, 'emerald_dragon.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Inferno Demon', 85, 75, 'inferno_demon.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Arctic Warrior', 70, 80, 'arctic_warrior.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Toxic Scorpion', 75, 65, 'toxic_scorpion.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Royal Guardian', 65, 90, 'royal_guardian.jpg');
+INSERT INTO cards (name, attack, defense, illustration) VALUES ('Obsidian Golem', 70, 95, 'obsidian_golem.jpg');
+
+SELECT * FROM cards;
+
+
+
+INSERT INTO series (name, releaseDate) VALUES ('Elemental Masters', '2025-03-03');
+INSERT INTO series (name, releaseDate) VALUES ('Shadow Realm', '2025-03-03');
+INSERT INTO series (name, releaseDate) VALUES ('Knights of Valor', '2025-03-03');
+INSERT INTO series (name, releaseDate) VALUES ('Mythic Beasts', '2025-03-03');
+INSERT INTO series (name, releaseDate) VALUES ('Woodland Guardians', '2025-03-03');
+INSERT INTO series (name, releaseDate) VALUES ('Ancient Constructs', '2025-03-03');
+INSERT INTO series (name, releaseDate) VALUES ('Frost Kingdom', '2025-03-03');
+INSERT INTO series (name, releaseDate) VALUES ('Inferno Lords', '2025-03-03');
+INSERT INTO series (name, releaseDate) VALUES ('Celestial Order', '2025-03-03');
+INSERT INTO series (name, releaseDate) VALUES ('Tribal Warriors', '2025-03-03');
+
+SELECT * FROM series;
+
+
+-- Elemental Masters (id: 1)
+INSERT INTO cards_series (cardId, seriesId) VALUES (5, 1);  -- Fire Elemental
+INSERT INTO cards_series (cardId, seriesId) VALUES (6, 1);  -- Water Sprite
+INSERT INTO cards_series (cardId, seriesId) VALUES (34, 1); -- Earth Elemental
+INSERT INTO cards_series (cardId, seriesId) VALUES (62, 1); -- Lightning Elemental
+INSERT INTO cards_series (cardId, seriesId) VALUES (79, 1); -- Lava Elemental
+
+-- Shadow Realm (id: 2)
+INSERT INTO cards_series (cardId, seriesId) VALUES (2, 2);  -- Shadow Assassin
+INSERT INTO cards_series (cardId, seriesId) VALUES (39, 2); -- Shadow Dragon
+INSERT INTO cards_series (cardId, seriesId) VALUES (58, 2); -- Phantom Assassin
+INSERT INTO cards_series (cardId, seriesId) VALUES (62, 2); -- Lightning Elemental
+INSERT INTO cards_series (cardId, seriesId) VALUES (67, 2); -- Spectral Knight
+INSERT INTO cards_series (cardId, seriesId) VALUES (87, 2); -- Shadow Priest
+
+-- Knights of Valor (id: 3)
+INSERT INTO cards_series (cardId, seriesId) VALUES (7, 3);  -- Knight Templar
+INSERT INTO cards_series (cardId, seriesId) VALUES (32, 3); -- Royal Paladin
+INSERT INTO cards_series (cardId, seriesId) VALUES (43, 3); -- Silver Knight
+INSERT INTO cards_series (cardId, seriesId) VALUES (67, 3); -- Spectral Knight
+INSERT INTO cards_series (cardId, seriesId) VALUES (94, 3); -- Arctic Warrior
+INSERT INTO cards_series (cardId, seriesId) VALUES (98, 3); -- Royal Guardian
+
+-- Mythic Beasts (id: 4)
+INSERT INTO cards_series (cardId, seriesId) VALUES (1, 4);  -- Dragon Guardian
+INSERT INTO cards_series (cardId, seriesId) VALUES (13, 4); -- Phoenix Guardian
+INSERT INTO cards_series (cardId, seriesId) VALUES (31, 4); -- Sea Serpent
+INSERT INTO cards_series (cardId, seriesId) VALUES (35, 4); -- Sky Wyvern
+INSERT INTO cards_series (cardId, seriesId) VALUES (76, 4); -- Noble Griffin
+INSERT INTO cards_series (cardId, seriesId) VALUES (39, 4); -- Shadow Dragon (also in Shadow Realm)
+
+-- Woodland Guardians (id: 5)
+INSERT INTO cards_series (cardId, seriesId) VALUES (3, 5);  -- Elven Archer
+INSERT INTO cards_series (cardId, seriesId) VALUES (18, 5); -- Forest Druid
+INSERT INTO cards_series (cardId, seriesId) VALUES (66, 5); -- Vine Monster
+INSERT INTO cards_series (cardId, seriesId) VALUES (80, 5); -- Forest Guardian
+INSERT INTO cards_series (cardId, seriesId) VALUES (91, 5); -- Crystal Fairy
+
+-- Ancient Constructs (id: 6)
+INSERT INTO cards_series (cardId, seriesId) VALUES (4, 6);  -- Stone Golem
+INSERT INTO cards_series (cardId, seriesId) VALUES (38, 6); -- Ancient Construct
+INSERT INTO cards_series (cardId, seriesId) VALUES (52, 6); -- Steel Sentinel
+INSERT INTO cards_series (cardId, seriesId) VALUES (63, 6); -- Enchanted Golem
+INSERT INTO cards_series (cardId, seriesId) VALUES (71, 6); -- Ice Queen
+INSERT INTO cards_series (cardId, seriesId) VALUES (99, 6); -- Obsidian Golem
+INSERT INTO cards_series (cardId, seriesId) VALUES (4, 3);  -- Stone Golem (also in Knights of Valor)
+
+-- Frost Kingdom (id: 7)
+INSERT INTO cards_series (cardId, seriesId) VALUES (11, 7); -- Frost Giant
+INSERT INTO cards_series (cardId, seriesId) VALUES (57, 7); -- Frost Mage
+INSERT INTO cards_series (cardId, seriesId) VALUES (71, 7); -- Ice Queen
+INSERT INTO cards_series (cardId, seriesId) VALUES (86, 7); -- Frost Demon
+INSERT INTO cards_series (cardId, seriesId) VALUES (94, 7); -- Arctic Warrior (also in Knights of Valor)
+
+-- Inferno Lords (id: 8)
+INSERT INTO cards_series (cardId, seriesId) VALUES (17, 8); -- Demon Overlord
+INSERT INTO cards_series (cardId, seriesId) VALUES (5, 8);  -- Fire Elemental (also in Elemental Masters)
+INSERT INTO cards_series (cardId, seriesId) VALUES (51, 8); -- Volcanic Drake
+INSERT INTO cards_series (cardId, seriesId) VALUES (79, 8); -- Lava Elemental (also in Elemental Masters)
+INSERT INTO cards_series (cardId, seriesId) VALUES (97, 8); -- Inferno Demon
+
+-- Celestial Order (id: 9)
+INSERT INTO cards_series (cardId, seriesId) VALUES (16, 9); -- Celestial Angel
+INSERT INTO cards_series (cardId, seriesId) VALUES (48, 9); -- Star Weaver
+INSERT INTO cards_series (cardId, seriesId) VALUES (73, 9); -- Sun Priest
+INSERT INTO cards_series (cardId, seriesId) VALUES (89, 9); -- Celestial Mage
+INSERT INTO cards_series (cardId, seriesId) VALUES (16, 2); -- Celestial Angel (also in Shadow Realm)
+
+-- Tribal Warriors (id: 10)
+INSERT INTO cards_series (cardId, seriesId) VALUES (19, 10); -- Minotaur Warrior
+INSERT INTO cards_series (cardId, seriesId) VALUES (29, 10); -- Barbarian Chief
+INSERT INTO cards_series (cardId, seriesId) VALUES (64, 10); -- War Chief
+INSERT INTO cards_series (cardId, seriesId) VALUES (80, 10); -- Forest Guardian
+INSERT INTO cards_series (cardId, seriesId) VALUES (93, 10); -- Tribal Shaman
+INSERT INTO cards_series (cardId, seriesId) VALUES (95, 10); -- Toxic Scorpion
+
+SELECT * FROM cards_series;
